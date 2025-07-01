916c2a9fc61b33528f41052e39d13b729cec2b5c
myanacondadontnun <104224428+myanacondadontnun@users.noreply.github.com>
2025-07-01 20:02:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,26 +12,10 @@ const nextConfig = {
       "ugc.same-assets.com",
     ],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "source.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "ext.same-assets.com", pathname: "/**" },
+      { protocol: "https", hostname: "ugc.same-assets.com", pathname: "/**" },
     ],
   },
 };
