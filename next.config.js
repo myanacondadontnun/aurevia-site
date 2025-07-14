698f0628fae85967040bffc706890dfdd62f4b64
unknown <ritwik018@gmail.com>
2025-07-14 18:39:31 +0100

    fixed deployment issues

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-
   images: {
-    unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
       "ext.same-assets.com",
       "ugc.same-assets.com",
+      "aurevia-content.s3.eu-north-1.amazonaws.com",
     ],
     remotePatterns: [
       { protocol: "https", hostname: "source.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "ext.same-assets.com", pathname: "/**" },
       { protocol: "https", hostname: "ugc.same-assets.com", pathname: "/**" },
+      { protocol: "https", hostname: "aurevia-content.s3.eu-north-1.amazonaws.com", pathname: "/**" },
     ],
   },
 };
